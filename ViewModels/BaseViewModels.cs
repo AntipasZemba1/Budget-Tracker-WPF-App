a99b61b12d6b023e6d1a74ba59dbb79029c1cf05
Antipas Zemba <antipasnzeba@gmail.com>
2025-08-03 13:43:53 -0700

    starting over

--- ViewModels/BaseViewModels.cs
+++
@@ -1,9 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace BudgetTracker.ViewModels
-{
-    public class BaseViewModel : ObservableObject
-    {
-        // ObservableObject gives us INotifyPropertyChanged support
-    }
-}
