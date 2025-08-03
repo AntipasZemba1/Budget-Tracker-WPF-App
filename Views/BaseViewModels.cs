4e3315f0b4d64fe7a088bf04f198bb0ac6898bdc
Antipas Zemba <antipasnzeba@gmail.com>
2025-08-03 13:23:37 -0700

    BaseViewModel.cs added to View Models

---
+++ Views/BaseViewModels.cs
@@ -0,0 +1,9 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BudgetTracker.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        // ObservableObject gives us INotifyPropertyChanged support
+    }
+}
